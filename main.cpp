bd44f36fd8bc8a6cc0ce513a15ede8c8fcc90570
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2024-04-05 13:33:53 +0300

    console visualization

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int set_0[5] = {18, 35, 49, 50, 51};
 class Field {
 public:
     Field(): field(new bool[WIDTH * HEIGHT]) {}
+
     void update() {
-        bool* new_field;
+        int alive_neighbors;
+        bool* new_field = new bool[WIDTH * HEIGHT];
         for (int i = 0; i < WIDTH * HEIGHT; i++)
-            new_field[i] = field[i];
+            *(new_field + i) = *(field + i);
         for (int i = 1; i < HEIGHT - 1; i++)
             for (int j = 1; j < WIDTH - 1; j++) {
                 alive_neighbors = 0;
@@ -20,7 +22,7 @@ public:
                 alive_neighbors -= field[i * WIDTH + j];
                 if (!field[i * WIDTH + j] && alive_neighbors == 3)
                     new_field[i * WIDTH + j] = true;
-                else if (field[i * WIDTH + j] && 2 <= alive_neighbors && alive_neighbors <= 3)
+                else if (field[i * WIDTH + j] && (2 > alive_neighbors || alive_neighbors > 3))
                     new_field[i * WIDTH + j] = false;
             }
         delete[] field;
@@ -39,6 +41,7 @@ public:
                         field[i * WIDTH + j] = true;
     }
     void show() {
+        std::cout << std::endl;
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++)
                 std::cout << field[i * WIDTH + j] << ' ';
@@ -48,7 +51,6 @@ public:
 
 private:
     bool* field;
-    int alive_neighbors;
 };
 
 
