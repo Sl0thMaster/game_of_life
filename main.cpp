25d360382ff1c643a77528a578055048c8fc4b39
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2024-04-05 12:39:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,44 +2,65 @@
 
 const int WIDTH = 16;
 const int HEIGHT = 9;
-int set_0[4] = {18, 22, 23, 24};
+int set_0[5] = {18, 35, 49, 50, 51};
 
 class Field {
 public:
     Field(): field(new bool[WIDTH * HEIGHT]) {}
-//    void update() {
-//
-//    }
+    void update() {
+        bool* new_field;
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+            new_field[i] = field[i];
+        for (int i = 1; i < HEIGHT - 1; i++)
+            for (int j = 1; j < WIDTH - 1; j++) {
+                alive_neighbors = 0;
+                for (int n_i = -1; n_i < 2; n_i++)
+                    for (int n_j = -1; n_j < 2; n_j++)
+                        alive_neighbors += field[(i + n_i) * WIDTH + j + n_j];
+                alive_neighbors -= field[i * WIDTH + j];
+                if (!field[i * WIDTH + j] && alive_neighbors == 3)
+                    new_field[i * WIDTH + j] = true;
+                else if (field[i * WIDTH + j] && 2 <= alive_neighbors && alive_neighbors <= 3)
+                    new_field[i * WIDTH + j] = false;
+            }
+        delete[] field;
+        field = new_field;
+    }
     void clear() {
         for (int i = 0; i < HEIGHT; i++)
             for (int j = 0; j < WIDTH; j++)
-                field[i * HEIGHT + j] = false;
+                field[i * WIDTH + j] = false;
     }
-    void set(int[] x) {
-        for (auto k: x)
+    void set(int* x, int size) {
+        for (int k = 0; k < size; k++)
             for (int i = 0; i < HEIGHT; i++)
                 for (int j = 0; j < WIDTH; j++)
-                    if (x[k] == i * HEIGHT + j)
-                        field[i * HEIGHT + j] = true;
+                    if (x[k] == i * WIDTH + j)
+                        field[i * WIDTH + j] = true;
     }
     void show() {
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++)
-                std::cout << field[i * HEIGHT + j] << ' ';
+                std::cout << field[i * WIDTH + j] << ' ';
             std::cout << std::endl;
         }
     }
 
 private:
     bool* field;
+    int alive_neighbors;
 };
 
 
 int main() {
     Field field;
     field.clear();
-    field.set(set_0);
+    field.set(set_0, std::size(set_0));
     field.show();
+    for (int i = 0; i < 5; i++) {
+        field.update();
+        field.show();
+    }
     return 0;
 }
 
