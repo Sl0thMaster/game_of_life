4dfe47425f162b29ae18ecb912d67b7824242cdb
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2024-04-07 22:56:27 +0300

    Update field.h

--- field.h
+++ field.h
@@ -1,20 +1,21 @@
-const int WIDTH = 26;
-const int HEIGHT = 18;
-
 class Field {
 public:
-    Field();
+    Field(int field_width, int field_height);
 
     bool& operator[](int index);
 
-    void update();
+    void advance();
 
     void toggle(int x, int y);
 
     void clear();
 
+    int* living_cells();
+
     void set(int* x, int size);
 
 private:
     bool* field;
+    int width;
+    int height;
 };
