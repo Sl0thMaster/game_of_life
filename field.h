26cba2c9b70d8ad4a5bd177f2a876cd15e195e3f
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2024-04-08 13:11:05 +0300

    Update field.h

--- field.h
+++ field.h
@@ -1,38 +1,55 @@
 #include <iostream>
 #include <fstream>
 
+/**
+ * \brief Класс, реализующий функционал поля для игры Жизнь.
+ */
 class Field {
 public:
     Field(int field_width, int field_height);
 
     Field(const Field& other);
 
-    Field(Field&& other) noexcept ;
+    Field(Field&& other) noexcept;
 
     Field& operator=(Field copy);
 
     ~Field();
 
+    //! Доступ к элементу поля по индексу.
     bool& operator[](int index);
 
+    //! Переход поля к следующему поколению.
     void advance();
 
+    //! Переключение состояния клетки.
     void toggle(int x, int y);
 
+    //! Получение значения клетки по её координатам.
     bool get(int x, int y);
 
+    //! Массив живых клеток.
     int* living_cells();
 
+    //! Загрузка состояния поля из файла.
     void load(const std::string& file_path);
 
+    //! Сохранение состояния поля в файл.
     void save(const std::string& file_path);
 
 private:
+    //! Поле игры.
     bool* field;
+
+    //! Ширина поля.
     int width;
+
+    //! Высота поля.
     int height;
 
+    //! Обмен полями.
     void swap(Field& other);
 
+    //! Очистка поля.
     void clear();
 };
