adf15c94076294e406b9bc7b69fe7a09a6cc3dfd
Andrey Alyamovsky <alyamovskyaa@gmail.com>
2024-04-08 03:23:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,14 +20,9 @@ int field_scale = 2;
 
 int main()
 {
+//    Поле
     Field field = Field(field_scale * WIDTH, field_scale * HEIGHT);
-    field.clear();
-    field.set(set_1, std::size(set_1));
-
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Game of life", sf::Style::Titlebar | sf::Style::Close);
-    window.setFramerateLimit(FPS);
-
-    sf::Event event;
+//    field.set(set_1, std::size(set_1));
 
     sf::RectangleShape cell(sf::Vector2f(SIDE_OF_SQUARE / field_scale, SIDE_OF_SQUARE / field_scale));
     cell.setFillColor(sf::Color::White);
@@ -35,6 +30,7 @@ int main()
     sf::RectangleShape pixel(sf::Vector2f(1, 1));
     pixel.setFillColor(sf::Color::Black);
 
+//    Меню
     sf::RectangleShape transparent(sf::Vector2f(2 * SIDE_OF_SQUARE, 2 * SIDE_OF_SQUARE));
     transparent.setFillColor(sf::Color(0, 0, 0, 128));
 
@@ -46,33 +42,57 @@ int main()
     text.setFillColor(sf::Color::White);
 
     sf::Sprite play;
-    sf::Sprite pause;
-    sf::Sprite speed1;
-    sf::Sprite speed2;
-    sf::Sprite frame;
-
     sf::Texture texture1;
     texture1.loadFromFile("textures/play.png");
     play.setTexture(texture1);
+    play.setPosition(27 * SIDE_OF_SQUARE,15 * SIDE_OF_SQUARE);
+
+    sf::Sprite pause;
     sf::Texture texture2;
     texture2.loadFromFile("textures/pause.png");
     pause.setTexture(texture2);
+    pause.setPosition(27 * SIDE_OF_SQUARE,15 * SIDE_OF_SQUARE);
+
+    sf::Sprite speed1;
+    sf::Sprite speed2;
     sf::Texture texture3;
     texture3.loadFromFile("textures/speed.png");
-
     speed1.setTexture(texture3);
     speed2.setTexture(texture3);
     speed1.rotate(180);
-    play.setPosition(27 * SIDE_OF_SQUARE,15 * SIDE_OF_SQUARE);
-    pause.setPosition(27 * SIDE_OF_SQUARE,15 * SIDE_OF_SQUARE);
     speed2.setPosition(29 * SIDE_OF_SQUARE,4 * SIDE_OF_SQUARE);
     speed1.setPosition(29 * SIDE_OF_SQUARE,6 * SIDE_OF_SQUARE);
     text.setPosition(28 * SIDE_OF_SQUARE,0);
 
+    sf::Sprite frame;
     sf::Texture texture4;
     texture4.loadFromFile("textures/frame.png");
     frame.setTexture(texture4);
     frame.setPosition(26 * SIDE_OF_SQUARE,0);
+
+    sf::Sprite button_frame;
+    sf::Texture texture5;
+    texture5.loadFromFile("textures/button_frame.png");
+    button_frame.setTexture(texture5);
+    sf::Text text1;
+    sf::Text text2;
+    text1.setFont(font);
+    text2.setFont(font);
+    text1.setCharacterSize(50);
+    text2.setCharacterSize(50);
+    text1.setFillColor(sf::Color::White);
+    text2.setFillColor(sf::Color::White);
+    text1.setPosition(27 * SIDE_OF_SQUARE + 12,11 * SIDE_OF_SQUARE + 10);
+    text2.setPosition(27 * SIDE_OF_SQUARE + 8,13 * SIDE_OF_SQUARE + 10);
+    text1.setString("Load");
+    text2.setString("Save");
+
+//    Окно
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Game of life",
+                            sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(FPS);
+
+    sf::Event event;
 
     while (window.isOpen())
     {
@@ -89,7 +109,8 @@ int main()
                     int mouse_x = m_position.x / SIDE_OF_SQUARE;
                     int mouse_y = m_position.y / SIDE_OF_SQUARE;
                     if (mouse_x < WIDTH)
-                        field.toggle(m_position.x / (SIDE_OF_SQUARE / field_scale), m_position.y / (SIDE_OF_SQUARE / field_scale));
+                        field.toggle(m_position.x / (SIDE_OF_SQUARE / field_scale),
+                                     m_position.y / (SIDE_OF_SQUARE / field_scale));
                     else if (27 <= mouse_x && mouse_x < 31 && 15 <= mouse_y && mouse_y < 17)
                         paused = !paused;
                     else if (29 <= mouse_x && mouse_x < 31 && 4 <= mouse_y && mouse_y < 6 && button2_available) {
@@ -102,7 +123,10 @@ int main()
                         if (game_speed == 1)
                             button1_available = false;
                         button2_available = true;
-                    }
+                    } else if (27 <= mouse_x && mouse_x < 31 && 11 <= mouse_y && mouse_y < 13)
+                        field.load("saves/load.txt");
+                    else if (27 <= mouse_x && mouse_x < 31 && 13 <= mouse_y && mouse_y < 15)
+                        field.save("saves/save.txt");
                 }
             }
         }
@@ -147,6 +171,13 @@ int main()
             transparent.setPosition(29 * SIDE_OF_SQUARE, 4 * SIDE_OF_SQUARE);
             window.draw(transparent);
         }
+//        Кнопки сохранения и загрузки
+        button_frame.setPosition(27 * SIDE_OF_SQUARE, 11 * SIDE_OF_SQUARE);
+        window.draw(button_frame);
+        button_frame.setPosition(27 * SIDE_OF_SQUARE, 13 * SIDE_OF_SQUARE);
+        window.draw(button_frame);
+        window.draw(text1);
+        window.draw(text2);
 
         window.display();
     }
